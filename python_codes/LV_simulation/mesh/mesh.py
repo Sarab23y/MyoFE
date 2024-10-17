6e82d03affade04d386dc5a41c66091243ac6790
Sarab23y <sba431@uky.edu>
2024-10-17 16:22:16 -0400

    d

--- python_codes/LV_simulation/mesh/mesh.py
+++ python_codes/LV_simulation/mesh/mesh.py
@@ -841,107 +841,122 @@ from ..dependencies.assign_heterogeneous_params import assign_heterogeneous_para
         Ecc = ecc[i]*Ea[i,j]*ecc[j]
 
         return Ell,Err,Ecc
-        
-import sys
 
-sys.path.append(r"C:\Users\sba431\Github\MyoFE\demos\Sara_test_cases\Bivetricular")
 
-import vtk_py as vtk_py
-import dolfin as dolfin
+
+from pyclbr import Function
 import numpy as np
+import json
+import dolfin as dolfin
+import os
+from ..dependencies.forms import Forms
+from ..dependencies.nsolver import NSolver
+from ..dependencies.assign_heterogeneous_params import assign_heterogeneous_params 
+import vtk_py
 from mpi4py import MPI as pyMPI
 
-# Parameters
-epicutfilename = "91690_EP2_2.stl"
-LVendocutfilename = "91690_LV2_2.stl"
-RVendocutfilename = "91690_RV2_2.stl"
-meshsize = 0.6
-casename = "BiV"
-epiid = 1
-lvid = 2
-rvid = 3
-isLV = False
-quad_deg = 4
-RVangle = [60, -60]
-LVangle = [60, -60]
-Septangle = [60, -60]
-quad_deg = 4
-isrotatept = False
-isreturn = True
-meshname = "91690"
-outdir = "./"
+class MeshClass():
 
+    def __init__(self, parent_parameters,
+                 predefined_mesh=None,
+                 predefined_functions=None):
 
-# Create VTK mesh
-ugrid = vtk_py.create_BiVmesh(
-    epicutfilename, LVendocutfilename, RVendocutfilename, casename, meshsize=meshsize
-)
+        self.parent_parameters = parent_parameters
+        self.hs = self.parent_parameters.hs
+        mesh_struct = parent_parameters.instruction_data['mesh']
 
-fenics_mesh_ref, fenics_facet_ref, fenics_edge_ref = vtk_py.extractFeNiCsBiVFacet(
-    ugrid, tol=1e-1
-)
+        if self.parent_parameters.comm.Get_size() > 1:
+            parameters['mesh_partitioner'] = 'SCOTCH'
 
-# Assigning material IDs
-pdata_endLV = vtk_py.readSTL(LVendocutfilename, verbose=False)
-pdata_endRV = vtk_py.readSTL(RVendocutfilename, verbose=False)
-pdata_epi = vtk_py.readSTL(epicutfilename, verbose=False)
-matid = vtk_py.addRegionsToBiV(ugrid, pdata_endLV, pdata_endRV, pdata_epi)
-cnt = 0
-for cell in dolfin.cells(fenics_mesh_ref):
-    idx = int(ugrid.GetCellData().GetArray("region_id").GetTuple(cnt)[0])
-    fenics_mesh_ref.domains().set_marker((cell.index(), idx), 3)
-    cnt += 1
-matid = dolfin.MeshFunction("size_t", fenics_mesh_ref, 3, fenics_mesh_ref.domains())
+        self.model = dict()
+        self.data = dict()
 
-# Translate mesh so that z = 0
-comm = fenics_mesh_ref.mpi_comm().tompi4py()
-ztop = comm.allreduce(np.amax(fenics_mesh_ref.coordinates()[:, 2]), op=pyMPI.MAX)
-ztrans = dolfin.Expression(("0.0", "0.0", str(-ztop)), degree=1)
+        # Check if a predefined mesh is given, else create a new mesh
+        if not predefined_mesh:
+            # New BiV mesh creation and facet extraction logic
+            ugrid = vtk_py.create_BiVmesh(
+                mesh_struct['epicutfilename'],
+                mesh_struct['LVendocutfilename'],
+                mesh_struct['RVendocutfilename'],
+                mesh_struct['casename'],
+                meshsize=mesh_struct.get('meshsize', 0.6)
+            )
 
-if dolfin.dolfin_version() != "1.6.0":
-    dolfin.ALE.move(fenics_mesh_ref, ztrans)
-else:
-    mesh.move(ztrans)
+            # Extract FEniCS mesh and facets
+            self.model['mesh'], fenics_facet_ref, fenics_edge_ref = vtk_py.extractFeNiCsBiVFacet(
+                ugrid, tol=1e-1
+            )
 
-dolfin.File("BiVFacet.pvd") << fenics_facet_ref
-dolfin.File("matid.pvd") << matid
+            # Assign material IDs
+            pdata_endLV = vtk_py.readSTL(mesh_struct['LVendocutfilename'], verbose=False)
+            pdata_endRV = vtk_py.readSTL(mesh_struct['RVendocutfilename'], verbose=False)
+            pdata_epi = vtk_py.readSTL(mesh_struct['epicutfilename'], verbose=False)
+            matid = vtk_py.addRegionsToBiV(ugrid, pdata_endLV, pdata_endRV, pdata_epi)
+            cnt = 0
+            for cell in dolfin.cells(self.model['mesh']):
+                idx = int(ugrid.GetCellData().GetArray("region_id").GetTuple(cnt)[0])
+                self.model['mesh'].domains().set_marker((cell.index(), idx), 3)
+                cnt += 1
 
-# Set Fiber Orientation
-fiber_angle_param = {
-    "mesh": fenics_mesh_ref,
-    "facetboundaries": fenics_facet_ref,
-    "LV_fiber_angle": LVangle,
-    "LV_sheet_angle": [0.1, -0.1],
-    "Septum_fiber_angle": Septangle,
-    "Septum_sheet_angle": [0.1, -0.1],
-    "RV_fiber_angle": RVangle,
-    "RV_sheet_angle": [0.1, -0.1],
-    "LV_matid": 0,
-    "Septum_matid": 1,
-    "RV_matid": 2,
-    "matid": matid,
-    "isrotatept": isrotatept,
-    "isreturn": isreturn,
-    "outfilename": meshname,
-    "outdirectory": outdir,
-    "epiid": epiid,
-    "rvid": rvid,
-    "lvid": lvid,
-    "degree": quad_deg,
-}
+            matid = dolfin.MeshFunction("size_t", self.model['mesh'], 3, self.model['mesh'].domains())
 
-ef, es, en = vtk_py.SetBiVFiber_Quad_PyQ(fiber_angle_param)
+            # Translate mesh so that z = 0
+            comm = self.model['mesh'].mpi_comm().tompi4py()
+            ztop = comm.allreduce(np.amax(self.model['mesh'].coordinates()[:, 2]), op=pyMPI.MAX)
+            ztrans = dolfin.Expression(("0.0", "0.0", str(-ztop)), degree=1)
+            dolfin.ALE.move(self.model['mesh'], ztrans)
 
-# Write to hdf5 file
-f = dolfin.HDF5File(fenics_mesh_ref.mpi_comm(), outdir + meshname + ".hdf5", "w")
-f.write(fenics_mesh_ref, meshname)
-f.close()
+            # Save mesh and facets to HDF5
+            hdf5_path = os.path.join(os.getcwd(), mesh_struct['meshname'] + ".hdf5")
+            f = dolfin.HDF5File(self.model['mesh'].mpi_comm(), hdf5_path, "w")
+            f.write(self.model['mesh'], mesh_struct['meshname'])
+            f.write(fenics_facet_ref, mesh_struct['meshname'] + "/" + "facetboundaries")
+            f.write(fenics_edge_ref, mesh_struct['meshname'] + "/" + "edgeboundaries")
+            f.write(matid, mesh_struct['meshname'] + "/" + "matid")
+            f.close()
 
-f = dolfin.HDF5File(fenics_mesh_ref.mpi_comm(), outdir + meshname + ".hdf5", "a")
-f.write(fenics_facet_ref, meshname + "/" + "facetboundaries")
-f.write(fenics_edge_ref, meshname + "/" + "edgeboundaries")
-f.write(matid, meshname + "/" + "matid")
-f.write(ef, meshname + "/" + "eF")
-f.write(es, meshname + "/" + "eS")
-f.write(en, meshname + "/" + "eN")
-f.close()
+        else:
+            self.model['mesh'] = predefined_mesh
+
+        # Initialize communicator and functions
+        self.comm = self.model['mesh'].mpi_comm()
+        self.model['function_spaces'] = self.initialize_function_spaces(mesh_struct)
+        self.model['functions'] = self.initialize_functions(mesh_struct, predefined_functions)
+        self.model['boundary_conditions'] = self.initialize_boundary_conditions()
+
+        # Set up weak form and solver parameters
+        self.model['Ftotal'], self.model['Jac'], \
+            self.model['uflforms'], self.model['solver_params'] = self.create_weak_form()
+
+        # Now, setup the fibers
+        fiber_angle_param = {
+            "mesh": self.model['mesh'],
+            "facetboundaries": self.model['functions']['facetboundaries'],
+            "LV_fiber_angle": mesh_struct.get("LVangle", [60, -60]),
+            "LV_sheet_angle": [0.1, -0.1],
+            "Septum_fiber_angle": mesh_struct.get("Septangle", [60, -60]),
+            "Septum_sheet_angle": [0.1, -0.1],
+            "RV_fiber_angle": mesh_struct.get("RVangle", [60, -60]),
+            "RV_sheet_angle": [0.1, -0.1],
+            "LV_matid": 0,
+            "Septum_matid": 1,
+            "RV_matid": 2,
+            "matid": matid,
+            "isrotatept": False,
+            "isreturn": True,
+            "outfilename": mesh_struct['meshname'],
+            "outdirectory": mesh_struct['outdir'],
+            "epiid": mesh_struct['epiid'],
+            "rvid": mesh_struct['rvid'],
+            "lvid": mesh_struct['lvid'],
+            "degree": mesh_struct.get("quad_deg", 4),
+        }
+
+        ef, es, en = vtk_py.SetBiVFiber_Quad_PyQ(fiber_angle_param)
+
+        # Save fiber orientations
+        f = dolfin.HDF5File(self.model['mesh'].mpi_comm(), hdf5_path, "a")
+        f.write(ef, mesh_struct['meshname'] + "/" + "eF")
+        f.write(es, mesh_struct['meshname'] + "/" + "eS")
+        f.write(en, mesh_struct['meshname'] + "/" + "eN")
+        f.close()
